f58bb4a4726de3624f7ba3e70162b5d1a92ba8df
Andrew Glines and nick wise <nick@>
2017-05-03 14:00:38 -0700

    Complete logic to handle both vowel- and consonant-initial words

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,26 @@
 //BACK END LOGIC
 function vowelCheck(currentChar){
-  console.log(currentChar);
   var isVowel = false;
-//  var vowels = ["a","e","i","o","u"];
   var vowel = "aeiou";
   var tempVowel = vowel.indexOf(currentChar);
-  console.log(tempVowel);
   if ( tempVowel >= 0 ) {
     isVowel = true;
     return isVowel;
-    // some boolean = True; reutrn that boolean
-    //yes this is a vowel
+  }
+  else if (tempVowel < 0 ) {
+    isVowel = false;
+    return isVowel;
   }
 }
 
-function wordBuilder(splitWord, tempArray) {
+
+function wordBuilder(splitWord, tempArray, suffix) {
   console.log("tempArray", tempArray);
     if (tempArray) {
-      var suffix = "way"
-      var wordFinal = splitWord.join("") + suffix;
+      var wordFinal = splitWord.join("") + tempArray.join("") + suffix;
       console.log(wordFinal);
 
     }
-    // suffix = "way"
-  // needs what's left of original string
-  // the initial consonant tempArray
-  // the PigLatinate "ay" string
-  // concatenate all these
 }
 
 function wordParse(userInput){
@@ -34,32 +28,37 @@ function wordParse(userInput){
   var tempArray = [];
   var splitWord = userInput.split("");
 
-  // console.log(splitWord[0]);
-  // FIRST CASE - vowel starts
-  // if firstchar in splitWord is vowel
-  //   keep orig userInput? and append -ay -> output
+
+// IF FIRST CHAR IS VOWEL
   if (vowelCheck(splitWord[0])) {
-      console.log("passed vowelCheck - ", splitWord);
-          // userInput.append(rule1);
-        wordBuilder(splitWord, tempArray);
+      console.log("1st char is vowel ", splitWord);
+      var suffix = "way";
+      wordBuilder(splitWord, tempArray, suffix);
     }
+// IF FIRST CHAR IS CONST
+  else {
+    var i = 0
+    console.log("1st char is consont", splitWord);
+    // 1. pop off now-first char, vowelcheck
+      tempArray[i] = splitWord[0];
+      splitWord.shift();
+      console.log(tempArray[i], splitWord);
+      var suffix = "ay"
+      wordBuilder(splitWord, tempArray, suffix);
+
+  }
+}
 
 // SECOND CASE - consonant starts
 // elseif firstchar in splitWord is NOT vowel
-  else if(splitWord[0] != "a") {
-    var i = 0;
-    //   1. pop firstchar off to tempArray
-    tempArray[i] = splitWord.shift();
-    //   2. if nextchar IS a vowel, -> goto ConstructPigLatin
-    if (splitWord[0] )
-    i+=1;
-    }
-
-          // var wordFinal = splitWord.join() + tempArray.join() + suffix;
-  // return output
-};
-
-
+    //
+    // var i = 0;
+    // //   1. pop firstchar off to tempArray
+    // tempArray[i] = splitWord.shift();
+    // //   2. if nextchar IS a vowel, -> goto ConstructPigLatin
+    // if (splitWord[0] )
+    // i+=1;
+    // }
 
 
 // ==========================
