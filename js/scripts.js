36e7f8d8aa48598d3870aae86b0a2cc464384943
Andrew Glines and nick wise <andrew+nick@>
2017-05-03 11:46:01 -0700

    Define the back end logic for single word input beginning with a vowel, using functions to subdivide work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,77 @@
+//BACK END LOGIC
+function vowelCheck(currentChar){
+  console.log(currentChar);
+  var isVowel = false;
+//  var vowels = ["a","e","i","o","u"];
+  var vowel = "aeiou";
+  var tempVowel = vowel.indexOf(currentChar);
+  console.log(tempVowel);
+  if ( tempVowel >= 0 ) {
+    isVowel = true;
+    return isVowel;
+    // some boolean = True; reutrn that boolean
+    //yes this is a vowel
+  }
+}
+
+function wordBuilder(splitWord, tempArray) {
+  console.log("tempArray", tempArray);
+    if (tempArray) {
+      var suffix = "way"
+      var wordFinal = splitWord.join() + tempArray.join() + suffix;
+      console.log(wordFinal);
+
+    }
+    // suffix = "way"
+  // needs what's left of original string
+  // the initial consonant tempArray
+  // the PigLatinate "ay" string
+  // concatenate all these
+}
+
+function wordParse(userInput){
+  userInput = userInput.toLowerCase();
+  var tempArray = [];
+  var splitWord = userInput.split("");
+
+  // console.log(splitWord[0]);
+  // FIRST CASE - vowel starts
+  // if firstchar in splitWord is vowel
+  //   keep orig userInput? and append -ay -> output
+  if (vowelCheck(splitWord[0])) {
+      console.log("passed vowelCheck - ", splitWord);
+          // userInput.append(rule1);
+        wordBuilder(splitWord, tempArray);
+    }
+
+// SECOND CASE - consonant starts
+// elseif firstchar in splitWord is NOT vowel
+  else if(splitWord[0] != "a") {
+    var i = 0;
+    //   1. pop firstchar off to tempArray
+    tempArray[i] = splitWord.shift();
+    //   2. if nextchar IS a vowel, -> goto ConstructPigLatin
+    if (splitWord[0] )
+    i+=1;
+    }
+  // return output
+};
+
+
+
+
+// ==========================
+//FRONT END LOGIC
 $(document).ready(function(){
-  $("form#form1").submit(function(event){
+  $("form#globalForm").submit(function(event){
     event.preventDefault();
-    var userInput = $("input#input1").val();
+    var userInput = $("input#words").val();
+
+    wordParse(userInput);
+
+    // $("p#result").append(output);
+
+
+
   });
 });
